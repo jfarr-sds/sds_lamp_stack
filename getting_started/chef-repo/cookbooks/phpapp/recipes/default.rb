7991c8b4cdb2847a9d0dfaa1a924df2da9eb2c29
Jeff Farr <moogah@gmail.com>
2015-04-16 22:01:43 -0400

    added cookbooks and dependencies for php

--- getting_started/chef-repo/cookbooks/phpapp/recipes/default.rb
+++ getting_started/chef-repo/cookbooks/phpapp/recipes/default.rb
@@ -14,8 +14,19 @@ apache_site "default" do
 end
 
 mysql_service 'default' do
-  port '3306'
+  port '3307' # todo: need to use this port because the "mysql" instance still gets installed on 3306
   version '5.5'
-  initial_root_password 'sds123'
+  initial_root_password 'password'
   action [:create, :start]
+end
+
+mysql_config 'default' do
+  cookbook 'phpapp'
+  source 'mysite.cnf.erb'
+  notifies :restart, 'mysql_service[default]'
+  action :create
+end
+
+mysql_client 'default' do
+  action :create
 end
