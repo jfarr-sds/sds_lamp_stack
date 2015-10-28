3844696db281dd6cc85d1fc77f6afea1aaece177
Jeff Farr <jfarr-sds@Jeffs-MacBook-Pro-2.local>
2015-10-28 11:00:51 -0400

    changed to use php5-fpm instead of mod_php

--- chef-repo/cookbooks/phpapp/recipes/default.rb
+++ chef-repo/cookbooks/phpapp/recipes/default.rb
@@ -7,9 +7,19 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# enable multiverse so we can get libapache2-mod-fastcgi
+apt_repository 'archive-ubuntu-multiverse' do
+  uri        'http://archive.ubuntu.com/ubuntu'
+  distribution 'trusty'
+  components ['multiverse']
+  deb_src 'true'
+end
+
 include_recipe "apache2"
 include_recipe "php"
-include_recipe "apache2::mod_php5"
+include_recipe "php5-fpm::install"
+include_recipe "apache2::mod_fastcgi"
+include_recipe "apache2::mod_speling"
 include_recipe "apache2::mod_proxy"
 
 
@@ -33,6 +43,31 @@ template '/etc/apache2/sites-available/prime.conf' do
   owner 'root'
   group 'root'
 end
+
+template '/etc/apache2/conf-available/php5-fpm.conf' do
+  source 'php5-fpm.conf.erb'
+  cookbook 'phpapp'
+  mode '0644'
+  owner 'root'
+  group 'root'
+end
+
+apache_module 'actions' do
+  enable true
+end
+
+apache_module 'fastcgi' do
+  enable true
+end
+
+apache_module 'alias' do
+  enable true
+end
+
+apache_conf 'php5-fpm' do
+  enable true
+end
+
 
 # Enable QA vhost
 link "/etc/apache2/sites-enabled/prime.conf" do
